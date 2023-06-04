f40b45f53b5b537aaa3ed0f63fd44cd88e66a0cf
Sovinka20 <65658584+Sovinka20@users.noreply.github.com>
2023-06-04 08:30:25 +0300

    Перенастройка получения данных

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import AppHeader from "./components/app-header/appHeader.jsx";
 import BurgerConstructor from "./components/burger-constructor/burgerConstructor";
 import BurgerIngredients from "./components/burger-ingredient/burgerIngredient";
-import { data } from "./data/data";
+import api from "./data/api";
 
 function App() {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    api
+      .getData()
+      .then((result) => {
+        setApiData(result.data);
+      })
+      .catch((error) => {
+        console.log("В процессе получения данных произошла ошибка.");
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="header">
@@ -12,8 +26,8 @@ function App() {
       </header>
       <div className="wrapper">
         <main className="main">
-          <BurgerIngredients ingredients={data} />
-          <BurgerConstructor ingredients={data} />
+          <BurgerIngredients ingredients={apiData} />
+          <BurgerConstructor ingredients={apiData} />
         </main>
       </div>
     </div>
---
+++ src/data/api.js
@@ -0,0 +1,26 @@
+const URL = "https://norma.nomoreparties.space/api/ingredients";
+class Api {
+  constructor(urlApi) {
+    this.urlApi = urlApi;
+  }
+
+  validationJson(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(res.status);
+  }
+
+  getData() {
+    return fetch(`${this.urlApi}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((res) => this.validationJson(res));
+  }
+}
+
+const api = new Api(URL);
+
+export default api;
