8feb2acd6bea0194a7492316c1cbd119b4008325
Sovinka20 <65658584+Sovinka20@users.noreply.github.com>
2023-09-09 18:55:18 +0300

    animate download order

--- src/components/order-details/orderDetails.tsx
+++ src/components/order-details/orderDetails.tsx
@@ -7,19 +7,27 @@ const OrderDetails = () => {
   const orderNumber = useSelector(getOrderNumbers);
   return (
     <div className={styles.container}>
-      <h3 className="text text_type_digits-large mb-8 mt-30">
-        {orderNumber || 0}
-      </h3>
-      <span className="text text_type_main-medium mb-15">
-        Идентификатор заказа
-      </span>
-      <img className="mb-15" src={popupIcon} alt="" />
-      <span className="text text_type_main-default mb-2">
-        Ваш заказ начали готовить
-      </span>
-      <span className="text text_type_main-default text_color_inactive mb-30">
-        Дождитесь готовности на орбитальной станции
-      </span>
+      {orderNumber ? (
+        <>
+          <h3 className="text text_type_digits-large mb-8 mt-30">
+            {orderNumber}
+          </h3>
+          <span className="text text_type_main-medium mb-15">
+            Идентификатор заказа
+          </span>
+          <img className="mb-15" src={popupIcon} alt="" />
+          <span className="text text_type_main-default mb-2">
+            Ваш заказ начали готовить
+          </span>
+          <span className="text text_type_main-default text_color_inactive mb-30">
+            Дождитесь готовности на орбитальной станции
+          </span>
+        </>
+      ) : (
+        <span className={`${styles.sendUser} text text_type_main-medium mb-15`}>
+          Ожидайте, ваш заказ оформляется...
+        </span>
+      )}
     </div>
   );
 };
